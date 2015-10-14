b664476d4964a84757de168e89e5540c35065df0
Ken Micklas <kmicklas@gmail.com>
2015-10-14 14:24:18 -0400

    Use Dom monad

--- src/View.hs
+++ src/View.hs
@@ -7,6 +7,7 @@ module View
   ) where
 
 import Control.Concurrent.MVar
+import Control.Monad.IO.Class
 import Data.Text
 import GHCJS.DOM.Document
 
@@ -23,13 +24,12 @@ data Element
 
 type View = Identified Text Element
 
-reactView :: Document -> MVar e -> s -> (e -> s -> s) -> (s -> View) -> IO ()
-reactView document events init update render = do
-  currentView <- newMVar $ Node [] []
-  react events init update $ makeRenderer currentView
-  where makeRenderer _ = effectView document . render
+reactView :: MVar e -> s -> (e -> s -> s) -> (s -> View) -> Dom ()
+reactView events init update render = do
+  currentView <- liftIO $ newMVar $ Node [] []
+  react events init update $ effectView . render
 
-effectView :: Document -> View -> IO ()
-effectView document new = do
-  Just body <- getBody document
+effectView :: View -> Dom ()
+effectView new = do
+  Just body <- askDocument >>= liftIO . getBody
   return ()
