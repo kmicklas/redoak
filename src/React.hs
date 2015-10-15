487de2d1ca6b95b3a7381b33a1fdeba5496bc453
Ken Micklas <kmicklas@gmail.com>
2015-10-15 00:42:01 -0400

    Make react use arbitrary MonadIO

--- src/React.hs
+++ src/React.hs
@@ -3,10 +3,12 @@ module React
   ) where
 
 import Control.Concurrent.MVar
+import Control.Monad.IO.Class
 
-react :: MVar e -> s -> (e -> s -> s) -> (s -> IO ()) -> IO ()
+react :: (MonadIO m) =>
+  MVar e -> s -> (e -> s -> s) -> (s -> m ()) -> m ()
 react events init update effect = do
-  event <- takeMVar events
+  event <- liftIO $ takeMVar events
   let state = update event init
   effect state
   react events state update effect
