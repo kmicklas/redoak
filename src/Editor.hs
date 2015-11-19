4750e2cb24d004023f07f6079dd8613bb74c3aeb
Ken Micklas <kmicklas@gmail.com>
2015-11-19 17:25:12 -0500

    Add debug for events and try to handle keys more correctly

--- src/Editor.hs
+++ src/Editor.hs
@@ -18,6 +18,7 @@ data State
     { mode :: !Mode
     , currentId :: !Word
     , cursor :: Cursor Word Char
+    , events :: [Event]
     }
   deriving (Eq, Show)
 
@@ -29,6 +30,7 @@ data Mode
 data Event
   = KeyDown Key
   | KeyPress Char
+  deriving (Eq, Ord, Show)
 
 data Key
   = ArrowLeft
@@ -43,8 +45,11 @@ data Key
   | Other Int
   deriving (Eq, Ord, Show)
 
+ignoredChars :: [Char]
+ignoredChars = "\ESC\r\n\b\t"
+
 initState :: State
-initState = State Normal 0 $ Cursor empty $ Path [] (0, 0)
+initState = State Normal 0 (Cursor empty $ Path [] (0, 0)) []
 
 applyEdit :: EditM Word Char -> (State -> State)
 applyEdit f s = s { cursor = c'
@@ -52,24 +57,27 @@ applyEdit f s = s { cursor = c'
                   }
   where (c', i') = runState (f $ cursor s) $ currentId s
 
-
 onEvent :: Event -> State -> State
+onEvent e s = onEvent' e $ s { events = e : events s }
 
-onEvent (KeyDown ArrowLeft)  s = s { cursor = shiftLeft  $ cursor s }
-onEvent (KeyDown ArrowRight) s = s { cursor = shiftRight $ cursor s }
+onEvent' :: Event -> State -> State
 
-onEvent (KeyDown Tab) s | mode s == Insert = applyEdit push s
-onEvent (KeyPress '\r') s | mode s == Insert = applyEdit pop s
-onEvent (KeyPress ' ') s | mode s == Insert = applyEdit (pop <=< push) s
+onEvent' (KeyPress c) s | c `elem` ignoredChars = s
 
-onEvent (KeyPress '\ESC') s = s
+onEvent' (KeyDown ArrowLeft)  s = s { cursor = shiftLeft  $ cursor s }
+onEvent' (KeyDown ArrowRight) s = s { cursor = shiftRight $ cursor s }
+
+onEvent' (KeyDown Tab) s | mode s == Insert = applyEdit push s
+onEvent' (KeyDown Enter) s | mode s == Insert = applyEdit pop s
+onEvent' (KeyPress ' ') s | mode s == Insert = applyEdit (pop <=< push) s
+
+onEvent' (KeyDown Escape) s = s
   { mode = case mode s of Normal -> Insert
                           Insert -> Normal
   }
-onEvent (KeyPress c) s | mode s == Insert = s
+onEvent' (KeyPress c) s | mode s == Insert = s
   { cursor = selectNoneEnd $ change (singleton $ Atom (currentId s) c) $ cursor s
   , currentId = currentId s + 1
   }
-onEvent (KeyDown _) s = s
 
-onEvent _ s = s
+onEvent' _ s = s
