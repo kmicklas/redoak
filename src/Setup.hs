a73359bb33756829cd6690022217b4ba7994956e
Ken Micklas <kmicklas@gmail.com>
2015-11-25 01:35:11 -0500

    Workaround for (broken?) WebkitGTK

--- src/Setup.hs
+++ src/Setup.hs
@@ -10,6 +10,9 @@ import GHCJS.DOM.Document (Document)
 import System.Environment
 
 #ifndef __GHCJS__
+import GHCJS.DOM.Document (createElement, getHead)
+import GHCJS.DOM.HTMLLinkElement (castToHTMLLinkElement, setRel, setHref)
+import GHCJS.DOM.Node (appendChild)
 import Graphics.UI.Gtk.WebKit.WebView (webViewGetWebSettings, webViewSetWebSettings)
 import System.Glib.Attributes (AttrOp((:=)), set)
 import System.Glib.Properties (newAttrFromBoolProperty)
@@ -19,25 +22,34 @@ import Paths_redoak
 
 withDocument :: (Document -> IO ()) -> IO ()
 withDocument main = wrap $ runWebGUI $ \ view -> do
+  Just doc <- webViewGetDomDocument view
 #ifndef __GHCJS__
   settings <- webViewGetWebSettings view
   let fileAccess = newAttrFromBoolProperty "enable-file-access-from-file-uris"
   set settings [fileAccess := True]
   webViewSetWebSettings view settings
+
+  Just head <- getHead doc
+  Just link <- createElement doc $ Just "link"
+  let style = castToHTMLLinkElement link
+  setRel style "stylesheet"
+  styleFile <- getDataUrl "style/redoak.css"
+  setHref style styleFile
+  appendChild head $ Just style
 #endif
-  webViewGetDomDocument view >>= (main . fromJust)
+  main doc
   where
 #ifdef __GHCJS__
     wrap = id
 #else
     wrap a = do
-      url <- getDataURL "native.html"
+      url <- getDataUrl "native.html"
       withArgs [url] a
 #endif
 
-getDataURL :: String -> IO String
+getDataUrl :: String -> IO String
 #ifdef __GHCJS__
-getDataURL = return
+getDataUrl = return
 #else
-getDataURL = fmap ("file://" ++) . getDataFileName
+getDataUrl = fmap ("file://" ++) . getDataFileName
 #endif
