39e3adaf7287edf18ae6e36e76d55e1c53084274
Ken Micklas <kmicklas@gmail.com>
2015-12-14 23:27:57 -0500

    Start attempting to show selection

--- src/UI.hs
+++ src/UI.hs
@@ -15,6 +15,7 @@ import Control.Monad.IO.Class
 import Data.Bifunctor
 import Data.List
 import Data.Sequence
+import Data.Sequences as SS
 import Data.Text
 import GHCJS.DOM.Document (Document, keyDown, keyPress)
 import GHCJS.DOM.EventM (on, preventDefault, stopPropagation, uiKeyCode, uiCharCode)
@@ -29,7 +30,7 @@ import View
 viewState :: State -> View
 viewState s = defaultLayout "editor" [] $ Node [contentView, statusView]
   where contentView = defaultLayout "content" [] $ Node [treeView]
-        treeView = viewTree ["content"] $ second fst $ cursor s
+        treeView = viewCursor $ cursor s
         statusView = defaultLayout "status" [] $ Node [pathView, modeView]
         pathView = defaultLayout "path" [] $ Atom $ pack $ pathString $ path $ cursor s
         modeView = defaultLayout "mode" [] $ Atom $ pack $ show $ mode s
@@ -37,10 +38,51 @@ viewState s = defaultLayout "editor" [] $ Node [contentView, statusView]
           Data.List.intercalate ", " $ (fmap show is) ++
           [show start ++ if start == end then "" else "-" ++ show end]
 
-viewTree :: (Show i) => [Text] -> Tree Text i -> View
-viewTree classes (T (id := e)) = defaultLayout (pack $ show id) classes $ case e of
-  Atom a  -> Atom  a
-  Node ts -> Node $ fmap (viewTree classes) ts
+-- | View a tree containing the selection
+viewCursor :: (Show i) => Cursor Text i -> View
+viewCursor (T ((id, sel) := e)) =
+  defaultLayout (pack $ show id) ["content"] $ case (e, sel) of
+    (Atom a, Select r) ->  makeSelection a r $ Atom
+    (Node ts, Select r) -> makeSelection ts r $ Node . fmap viewTree
+    (Node ts, Descend i) -> Node $ mconcat $
+      [ fmap viewTree lPart
+      , fmap viewCursor selPart
+      , fmap viewTree rPart]
+      where (lPart, selPart, rPart) = split ts $ (i, i + 1)
+
+  where makeSelection :: IsSequence s
+                      => s -> Range
+                      -> (s -> Element Text View)
+                      -> Element Text View
+        makeSelection s r view = Node
+          [ defaultLayout "left-of-selection"   [] $ view lPart
+          , defaultLayout "selection" [dirClass r] $ view selPart
+          , defaultLayout "right-of-selection"  [] $ view rPart
+          ] where (lPart, selPart, rPart) = split s r
+
+        split :: IsSequence s => s -> Range -> (s, s, s)
+        split s (start, end) =
+          ( SS.take (fromIntegral front) s
+          , SS.take (fromIntegral $ back - front) $
+              SS.drop (fromIntegral front) s
+          , SS.drop (fromIntegral back) s
+          ) where front = min start end
+                  back  = max start end
+
+        dirClass (start, end) =
+          case compare start end of
+            LT -> "cursor-at-front"
+            GT -> "cursor-at-back"
+            EQ -> "empty-selection"
+
+-- | View a tree which doesn't contain the selection
+viewTree :: (Show i) => Cursor Text i -> View
+viewTree (T ((id, _) := e)) =
+  defaultLayout (pack $ show id) classes $ case e of
+    Atom a  -> Atom a
+    Node ts -> Node $ fmap viewTree ts
+
+  where classes = ["content"]
 
 defaultLayout :: Text -> [Text] -> Element Text View -> View
 defaultLayout id classes = T . (ViewInfo id classes (Width 0, Height 0) (X 0, Y 0) :=)
