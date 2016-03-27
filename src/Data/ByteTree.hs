b4aecef3caf515aae7ec71c6ddc4c1e1b012cba0
John Ericson <Ericson2314@Yahoo.com>
2016-03-27 10:06:39 -0700

    Delete empty module

--- src/Data/ByteTree.hs
+++
@@ -1,8 +0,0 @@
-module Data.ByteTree where
-
-import Control.Monad.Free
-import Data.ByteString
-import Data.Sequence
-
-type ByteNode = Free Seq ByteString
-newtype ByteTree = ByteTree (Seq ByteNode)
