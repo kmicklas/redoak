6b18e55afa9fd34d1f7d8395b425ca21760c5ab9
John Ericson <Ericson2314@Yahoo.com>
2016-04-03 09:54:47 -0700

    Don't recreate some top-level-ish divs every event

--- src/Redoak/UI.hs
+++ src/Redoak/UI.hs
@@ -9,6 +9,7 @@ module Redoak.UI
 
 import           Control.Monad.Identity
 import           Data.List
+import           Data.List.NonEmpty
 import           Data.Text (Text)
 import qualified Data.Text as T
 import           GHCJS.DOM.Document (Document)
@@ -24,22 +25,11 @@ import           Redoak.Tree.Range
 import           Redoak.View
 
 
-viewState :: MonadWidget t m => Editor -> m ()
-viewState s = divId "editor" $ do
-  divId "content" $
-    makeNode $ runIdentity $ layout $ cursor s
-  divId "status" $ do
-    let pathString (is, (start, end)) =
-          Data.List.intercalate ", " $ (fmap show is) ++
-          [show start ++ if start == end then "" else "-" ++ show end]
-    spanId "path" $ T.pack $ pathString $ path $ cursor s
-    spanId "mode" $ T.pack $ show $ mode s
-
 divId :: MonadWidget t m => String -> m a -> m a
 divId id = elAttr "div" ("id" =: id)
 
-spanId :: MonadWidget t m => String -> Text -> m ()
-spanId id = elAttr "span" ("id" =: id) . text . T.unpack
+spanId :: MonadWidget t m => String -> m a -> m a
+spanId id = elAttr "span" ("id" =: id)
 
 
 defaultLayout :: Text -> [Text] -> Element Text View -> View
@@ -47,8 +37,18 @@ defaultLayout id classes =
   (ViewInfo (Just id) classes (W 0, H 0) (X 0, Y 0) :<)
 
 runEditor :: MonadWidget t m => Document -> m ()
-runEditor doc = do
-  keys <- globalKeyEvents doc
+runEditor doc = editor =<< globalKeyEvents doc
+
+editor :: MonadWidget t m => Event t (NonEmpty KeyEvent) -> m ()
+editor keys = do
   stateStream <- foldDyn (flip $ foldl (flip handleEvent)) initState keys
-  _ <- dyn =<< mapDyn viewState stateStream
-  return ()
+  divId "editor" $ do
+    divId "content" $ do
+      _ <- dyn =<< mapDyn (makeNode . runIdentity . layout . cursor) stateStream
+      return ()
+    divId "status" $ do
+      let pathString (is, (start, end)) =
+            Data.List.intercalate ", " $ (fmap show is) ++
+            [show start ++ if start == end then "" else "-" ++ show end]
+      spanId "path" $ dynText =<< mapDyn (pathString . path . cursor) stateStream
+      spanId "mode" $ dynText =<< mapDyn (show . mode) stateStream
