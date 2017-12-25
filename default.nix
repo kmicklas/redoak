0c369bb03dd83325bfd79f08878ffca2b8bae938
Ken Micklas <kmicklas@gmail.com>
2017-12-24 23:30:25 -0500

    Use new reflex-platform project system

--- default.nix
+++ default.nix
@@ -1,1 +1,24 @@
-(import <nixpkgs> {}).lib.mapAttrs (_: p: p.redoak) (import ./env.nix)
+let
+
+reflex-platform = import ((import <nixpkgs> {}).fetchFromGitHub {
+  owner = "reflex-frp";
+  repo = "reflex-platform";
+  rev = "9d846b154bbceb6cc4a6495b3e8d8ea74bb0f7c1";
+  sha256 = "0n1m2pbb3qc23abivcr3gw518gkq05mvs71ky4ragp03r5xi0nhs";
+}) {};
+
+in
+
+reflex-platform.project ({ pkgs, ... }: {
+  packages = {
+    redoak = ./.;
+  };
+
+  shells = {
+    ghc = ["redoak"];
+    ghcjs = ["redoak"];
+  };
+
+  android = {};
+  ios = {};
+})
